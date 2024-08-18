e73dba5acdff17635dc36d2d0ca2174632a0318e
raviraj226 <avtar226@gmail.com>
2024-08-18 22:14:43 +0530

    changes

--- Python_code/raviraj.py
+++ Python_code/raviraj.py
@@ -1,1 +1,31 @@
-print("Raviraj")
+#normal practice
+name = "Raviraj Verma"
+age = 33
+print("My name is:",name)
+print("My age is:", age)
+
+#type of variables
+print(type(name))
+print(type(age))
+old = False
+a = None
+print(type(old))
+print(type(a))
+
+#arithmetic operators.
+a = 10
+b = 5
+add = a+b
+subs = a-b
+multi = a*b                      # (a) & (b) are operants.
+devide = a/b                     # (+)(-)(*)(/) are operators.
+mod = a%b                        # modulor operator(%)remainder.
+rem = a**b                       # power operator (**) to the power (a^B).
+print("Addition of a&b:",add)
+print("substrction of a&b:",subs)
+print("multiplication of a&b:",multi)
+print("Division of a&b:",devide)
+print("modulor of a&b:",mod)
+print("remainder b/w a&b:",rem)
+
+#relational operators.
